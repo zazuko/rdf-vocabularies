bf2e22739916ffbab1c79375d5ba8cdabcca426e
Victor Felder <victor@draft.li>
2019-05-06 14:01:47 +0200

    update ontologies

--- overrides.js
+++ overrides.js
@@ -1,68 +1,23 @@
 module.exports = {
-  as: {
-    skip: true
-  },
-  v: {
-    skip: true
-  },
-  xhv: {
-    skip: true
-  },
-  ical: {
-    skip: true
-  },
-  cc: {
-    file: 'https://creativecommons.org/schema.rdf'
-  },
-  rev: {
-    skip: true
-  },
-  wdr: {
-    skip: true
-  },
-  wdrs: {
-    skip: true
-  },
-  ctag: {
-    skip: true
-  },
-  xml: {
-    skip: true
-  },
-  rif: {
-    skip: true
-  },
-  sf: {
-    skip: true
-  },
-  gml: {
-    skip: true
-  },
-  xsd: {
-    skip: true
-  },
-  rss: {
-    file: 'http://purl.org/rss/1.0/schema.rdf'
-  },
-  dbp: {
-    file: 'http://dbpedia.org/data4/.ttl'
-  },
-  gtfs: {
-    mediaType: 'text/n3'
-  },
-  schema: {
-    file: 'http://schema.org/version/latest/schema.nt'
-  },
-  qb: {
-    mediaType: 'text/turtle'
-  },
-  void: {
-    file: 'http://vocab.deri.ie/void.ttl'
-  },
-  geo: {
-    file: 'http://www.modelservers.org/public/ontologies/vcon/geosparql_vocab_all.rdf'
-  },
-  sioc: {
-    skip: true
-  }
+  as: { skip: true },
+  cc: { file: 'https://creativecommons.org/schema.rdf' },
+  ctag: { skip: true },
+  geo: { file: 'http://www.modelservers.org/public/ontologies/vcon/geosparql_vocab_all.rdf' },
+  gml: { skip: true },
+  gtfs: { mediaType: 'text/n3' },
+  ical: { skip: true },
+  qb: { mediaType: 'text/turtle' },
+  rev: { skip: true },
+  rif: { skip: true },
+  rss: { file: 'http://purl.org/rss/1.0/schema.rdf' },
+  schema: { file: 'http://schema.org/version/latest/schema.nt' },
+  sf: { skip: true },
+  sioc: { skip: true },
+  v: { skip: true },
+  void: { file: 'http://vocab.deri.ie/void.ttl' },
+  wdr: { skip: true },
+  wdrs: { skip: true },
+  xhv: { skip: true },
+  xml: { skip: true },
+  xsd: { skip: true }
 }
