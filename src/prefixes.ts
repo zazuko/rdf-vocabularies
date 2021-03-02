cadf179d2ec158df98dbf8f67e37a8965b39bda3
Nishad Thalhath <nishad@users.noreply.github.com>
2021-03-02 15:45:49 +0900

    Add ShEx prefix

--- src/prefixes.ts
+++ src/prefixes.ts
@@ -54,6 +54,7 @@ const prefixes = {
   sdmx: 'http://purl.org/linked-data/sdmx#',
   sf: 'http://www.opengis.net/ont/sf#',
   sh: 'http://www.w3.org/ns/shacl#',
+  shex: 'http://www.w3.org/ns/shex#',
   sioc: 'http://rdfs.org/sioc/ns#',
   skos: 'http://www.w3.org/2004/02/skos/core#',
   skosxl: 'http://www.w3.org/2008/05/skos-xl#',
