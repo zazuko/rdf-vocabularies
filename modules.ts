1900aaf37d9086922596cd5159a96aeafce778a3
Victor Felder <victor@draft.li>
2021-03-08 11:30:24 +0100

    fix: escape backslashes in literals

--- modules.ts
+++ modules.ts
@@ -1,12 +1,12 @@
 import RdfjsSerializer from '@rdfjs/serializer-rdfjs'
-import DatasetExt from 'rdf-ext/lib/Dataset'
 import fs from 'fs'
 import { vocabularies } from './src'
+import QuadExt from 'rdf-ext/lib/Quad'
 
 const rdfjsSerializer = new RdfjsSerializer({ module: 'ts' })
 
-async function serializeRdfjsModule (prefix: string, dataset: DatasetExt) {
-  const code = rdfjsSerializer.transform(dataset.toArray())
+async function serializeRdfjsModule (prefix: string, quads: QuadExt[]) {
+  const code = rdfjsSerializer.transform(quads)
   const path = `./src/datasets/${prefix}.ts`
 
   fs.writeFileSync(path, code)
@@ -14,10 +14,18 @@ async function serializeRdfjsModule (prefix: string, dataset: DatasetExt) {
 
 async function main () {
   const datasets = await vocabularies()
-
   const codeModules = Object.entries(datasets)
     .map(([prefix, dataset]) => {
-      return serializeRdfjsModule(prefix, dataset)
+      const quadArray = dataset.toArray()
+      for (const quad of quadArray) {
+        if (quad.object.termType === 'Literal' &&
+          !quad.object.value.includes('\\\\') &&
+          quad.object.value.includes('\\')) {
+          quad.object.value = quad.object.value.replace(/\\/g, '\\\\')
+          quad.object.value = quad.object.value.replace(/\\\\n/g, '\\n')
+        }
+      }
+      return serializeRdfjsModule(prefix, quadArray)
         .then(() => console.log(`Wrote module for ${prefix}`))
         .catch(e => console.error(`Failed to serialize ${prefix}: ${e.message}`))
     })
