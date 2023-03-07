f72e2015600a982919313f527cf2daf69cd8b4d6
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-03-07 16:59:50 +0100

    feat: created wdrs package

--- packages/prefixes/lib/prefixesOnly.ts
+++ packages/prefixes/lib/prefixesOnly.ts
@@ -4,6 +4,5 @@ export default {
   sf: 'http://www.opengis.net/ont/sf#',
   v: 'http://rdf.data-vocabulary.org/#',
   wdr: 'http://www.w3.org/2007/05/powder#',
-  wdrs: 'http://www.w3.org/2007/05/powder-s#',
   xml: 'http://www.w3.org/XML/1998/namespace/',
 }
