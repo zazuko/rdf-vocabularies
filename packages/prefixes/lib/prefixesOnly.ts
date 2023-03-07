1812eafafa3989b3d40ed89d47f6323a8e6e355c
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-03-07 17:13:34 +0100

    feat: created gml and sf package

--- packages/prefixes/lib/prefixesOnly.ts
+++ packages/prefixes/lib/prefixesOnly.ts
@@ -1,7 +1,5 @@
 export default {
-  gml: 'http://www.opengis.net/ont/gml#',
   rif: 'http://www.w3.org/2007/rif#',
-  sf: 'http://www.opengis.net/ont/sf#',
   v: 'http://rdf.data-vocabulary.org/#',
   wdr: 'http://www.w3.org/2007/05/powder#',
   xml: 'http://www.w3.org/XML/1998/namespace/',
